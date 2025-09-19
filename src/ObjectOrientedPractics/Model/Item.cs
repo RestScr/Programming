0181afc47a086f6756e364f7ca286c5b710aa9cb
rebulo <mihail_k06@mail.ru>
2025-09-19 16:19:06 +0700

    Created item attributes

---
+++ src/ObjectOrientedPractics/Model/Item.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Item
+    {
+        private readonly int _id;
+        private string _name;
+        private string _info;
+        private double _cost;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public string Info
+        {
+            get
+            {
+                return _info;
+            }
+        }
+
+        public double Cost
+        {
+            get
+            {
+                return _cost;
+            }
+        }
+    }
+}
