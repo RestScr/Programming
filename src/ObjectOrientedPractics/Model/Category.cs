9850a2373be54ff0580410b53b16de185837ab72
rebulo <mihail_k06@mail.ru>
2025-10-06 13:17:26 +0700

    Added xml-comment

--- src/ObjectOrientedPractics/Model/Category.cs
+++ src/ObjectOrientedPractics/Model/Category.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ObjectOrientedPractics.Model
 {
+    /// <summary>
+    /// Перечисление категории
+    /// </summary>
     public enum Category
     {
         Notebook,
