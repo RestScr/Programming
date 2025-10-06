3663077913f983ba04069636f28b881313d22ab6
rebulo <mihail_k06@mail.ru>
2025-10-06 17:54:04 +0700

    Added load event to main form

--- src/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ src/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             tabControl1 = new TabControl();
             ItemsTab = new TabPage();
-            itemsTab1 = new ObjectOrientedPractics.View.Tabs.ItemsTab();
+            ItemsControlTab = new ObjectOrientedPractics.View.Tabs.ItemsTab();
             CustomersTab = new TabPage();
-            customerTab1 = new ObjectOrientedPractics.View.Tabs.CustomerTab();
+            CustomersControlTab = new ObjectOrientedPractics.View.Tabs.CustomerTab();
             tabControl1.SuspendLayout();
             ItemsTab.SuspendLayout();
             CustomersTab.SuspendLayout();
@@ -45,54 +45,55 @@
             tabControl1.Location = new Point(0, 0);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(898, 525);
+            tabControl1.Size = new Size(932, 525);
             tabControl1.TabIndex = 0;
             // 
             // ItemsTab
             // 
-            ItemsTab.Controls.Add(itemsTab1);
+            ItemsTab.Controls.Add(ItemsControlTab);
             ItemsTab.Location = new Point(4, 29);
             ItemsTab.Name = "ItemsTab";
             ItemsTab.Padding = new Padding(3);
-            ItemsTab.Size = new Size(890, 492);
+            ItemsTab.Size = new Size(924, 492);
             ItemsTab.TabIndex = 0;
             ItemsTab.Text = "Items";
             ItemsTab.UseVisualStyleBackColor = true;
             // 
-            // itemsTab1
+            // ItemsControlTab
             // 
-            itemsTab1.AutoSize = true;
-            itemsTab1.Location = new Point(3, 3);
-            itemsTab1.Name = "itemsTab1";
-            itemsTab1.Size = new Size(889, 468);
-            itemsTab1.TabIndex = 0;
+            ItemsControlTab.AutoSize = true;
+            ItemsControlTab.Location = new Point(3, 3);
+            ItemsControlTab.Name = "ItemsControlTab";
+            ItemsControlTab.Size = new Size(915, 491);
+            ItemsControlTab.TabIndex = 0;
             // 
             // CustomersTab
             // 
-            CustomersTab.Controls.Add(customerTab1);
+            CustomersTab.Controls.Add(CustomersControlTab);
             CustomersTab.Location = new Point(4, 29);
             CustomersTab.Name = "CustomersTab";
             CustomersTab.Padding = new Padding(3);
-            CustomersTab.Size = new Size(890, 492);
+            CustomersTab.Size = new Size(924, 492);
             CustomersTab.TabIndex = 1;
             CustomersTab.Text = "Customers";
             CustomersTab.UseVisualStyleBackColor = true;
             // 
-            // customerTab1
+            // CustomersControlTab
             // 
-            customerTab1.Location = new Point(3, 3);
-            customerTab1.Name = "customerTab1";
-            customerTab1.Size = new Size(884, 486);
-            customerTab1.TabIndex = 0;
+            CustomersControlTab.Location = new Point(3, 3);
+            CustomersControlTab.Name = "CustomersControlTab";
+            CustomersControlTab.Size = new Size(884, 486);
+            CustomersControlTab.TabIndex = 0;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(896, 524);
+            ClientSize = new Size(931, 524);
             Controls.Add(tabControl1);
             Name = "MainForm";
             Text = "Project Oriented Practics";
+            Load += MainForm_Load;
             tabControl1.ResumeLayout(false);
             ItemsTab.ResumeLayout(false);
             ItemsTab.PerformLayout();
@@ -105,7 +106,7 @@
         private TabControl tabControl1;
         private TabPage ItemsTab;
         private TabPage CustomersTab;
-        private View.Tabs.ItemsTab itemsTab1;
-        private View.Tabs.CustomerTab customerTab1;
+        private View.Tabs.ItemsTab ItemsControlTab;
+        private View.Tabs.CustomerTab CustomersControlTab;
     }
 }
--- src/ObjectOrientedPractics/View/MainForm.cs
+++ src/ObjectOrientedPractics/View/MainForm.cs
@@ -1,10 +1,35 @@
+using ObjectOrientedPractics.Model;
+
 namespace ObjectOrientedPractics
 {
     public partial class MainForm : Form
     {
+        private Store _shop = new Store();
+
+        /// <summary>
+        /// Свойство магазина главной формы, который хранит списки товаров и покупателей.
+        /// </summary>
+        public Store Shop
+        {
+            get
+            {
+                return _shop;
+            }
+            set
+            {
+                _shop = value;
+            }
+        }
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CustomersControlTab.Customers = Shop.Customers;
+            ItemsControlTab.Items = Shop.Items;
         }
     }
 }
