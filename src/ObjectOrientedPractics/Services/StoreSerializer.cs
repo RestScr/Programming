c43b70acc61228900d376dc36acb7966503b5c3d
rebulo <mihail_k06@mail.ru>
2025-10-06 18:05:32 +0700

    Created Serialization class

--- src/ObjectOrientedPractics/Services/StoreSerializer.cs
+++ src/ObjectOrientedPractics/Services/StoreSerializer.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ObjectOrientedPractics.Model;
 
 namespace ObjectOrientedPractics.Services
 {
-    internal class StoreSerializer
+    /// <summary>
+    /// Класс для сериализации, десериализации, загрузки и выгрузки данных из файла.
+    /// </summary>
+    public static class StoreSerializer
     {
     }
 }
