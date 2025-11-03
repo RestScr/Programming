2e7f2ef979e47b993948af55d5c6c515f9120393
rebulo <mihail_k06@mail.ru>
2025-11-03 15:12:30 +0700

    Added Order class and OrderStatus enumeration

--- src/ObjectOrientedPractics/Model/Cart.cs
+++ src/ObjectOrientedPractics/Model/Cart.cs
@@ -32,6 +32,5 @@ namespace ObjectOrientedPractics.Model
                 return cost;
             }
         }
-
     }
 }
---
+++ src/ObjectOrientedPractics/Model/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    /// <summary>
+    /// Класс заказа.
+    /// </summary>
+    internal class Order
+    {
+        /// <summary>
+        /// Поле класса, отвечающее за общее количество созданных экземпляров.
+        /// </summary>
+        private static readonly int _created = 0;
+
+        /// <summary>
+        /// Свойство созданных экземпляров класса.
+        /// </summary>
+        private static int Created
+        {
+            get
+            {
+                return _created;
+            }
+        }
+
+        /// <summary>
+        /// Идентификатор класса.
+        /// </summary>
+        public int Id { get; init; }
+    }
+}
