20637b8ecee1a6ba8f564f00351ece119caabb94
rebulo <mihail_k06@mail.ru>
2026-03-08 15:39:13 +0700

    Realised Contact class

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        /// <summary>
+        /// Изменение данных.
+        /// </summary>
+        public virtual bool Set<T>(ref T Field, T Value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(Field, Value))
+            {
+                return false;
+            }
+            else
+            {
+                Field = Value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                Set(ref _name, value);
+            }
+        }
+
+        private string _phoneNumber;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
--- src/Contacts/View/ViewModel/MainVM.cs
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using View.Model;
 
 namespace View.ViewModel
 {
-    class MainVM
+    public class MainVM : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private Contact _selectedContact;
+
+        public Contact SelectedContact
+        {
+            get => _selectedContact;
+            set 
+            { 
+                _selectedContact = value;
+                OnPropertyChanged(nameof(SelectedContact));
+            }
+        }
+        public MainVM()
+        {
+            SelectedContact = new Contact("Rostislav", "804939393", "fsfsdfssdfdfsfds");
+        }
     }
 }
