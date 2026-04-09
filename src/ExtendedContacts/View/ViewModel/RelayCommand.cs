7d71c87baf1e8eb9726d5d3c9f97be1210a353bc
rebulo <mihail_k06@mail.ru>
2026-04-09 19:02:27 +0700

    Deleted unused namespaces

--- src/ExtendedContacts/View/ViewModel/RelayCommand.cs
+++ src/ExtendedContacts/View/ViewModel/RelayCommand.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using View.Model;
+﻿using System.Windows.Input;
+
 
 namespace View.ViewModel.Commands;
 
