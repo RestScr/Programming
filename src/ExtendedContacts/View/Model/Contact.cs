ab959a186ee83ba4addb54f231146b20c2229d4a
rebulo <mihail_k06@mail.ru>
2026-04-09 17:30:43 +0700

    Fixed some errors

--- src/ExtendedContacts/View/Model/Contact.cs
+++ src/ExtendedContacts/View/Model/Contact.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Threading.Tasks;
 
