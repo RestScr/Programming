dc09dcf76da21d43ada10523a5be1cf6a5f4dcbe
rebulo <mihail_k06@mail.ru>
2026-04-09 19:05:21 +0700

    Deleted another unused namespaces

--- src/ExtendedContacts/View/Model/Contact.cs
+++ src/ExtendedContacts/View/Model/Contact.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace View.Model;
 
