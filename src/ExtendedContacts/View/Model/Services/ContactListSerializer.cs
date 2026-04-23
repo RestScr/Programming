ad9675bc4b2569b732087df17c9e8920b24b6146
rebulo <mihail_k06@mail.ru>
2026-04-23 20:58:15 +0700

    Removed unused namespaces

--- src/ExtendedContacts/View/Model/Services/ContactListSerializer.cs
+++ src/ExtendedContacts/View/Model/Services/ContactListSerializer.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace View.Model.Services;
--- src/ExtendedContacts/View/ViewModel/MainVM.cs
+++ src/ExtendedContacts/View/ViewModel/MainVM.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Data;
 using View.Model;
-using View.Model.Comparators;
 using View.Model.Services;
 using View.ViewModel.Commands;
 
