f288d7f803caa8f4ecc503398851ba164296b60b
rebulo <mihail_k06@mail.ru>
2026-04-26 22:13:06 +0700

    Replaced some code with ObservableProperty attribute

--- src/ExtendedContacts/View/Model/Contact.cs
+++ src/ExtendedContacts/View/Model/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 
 namespace View.Model;
@@ -7,113 +8,51 @@ namespace View.Model;
 /// <summary>
 /// Класс контакта.
 /// </summary>
-public class Contact : ICloneable, IEquatable<Contact>, INotifyPropertyChanged
+public partial class Contact : ObservableObject, ICloneable, IEquatable<Contact>
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    /// <summary>
-    /// Функция, вызывающее событие изменения свойства.
-    /// </summary>
-    /// <param name="propertyName"></param>
-    /// <returns> Всегда true. </returns>
-    public bool OnPropertyChanged([CallerMemberName] string propertyName = "")
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        return true;
-    }
-
     /// <summary>
     /// Статическое поле, хранящее количество созданных экземпляров.
     /// </summary>
-    private static int _created = 0;
+    private static uint _created = 0;
 
-    /// <summary>
-    /// Поле идентификатора экземпляра.
-    /// </summary>
-    private int _id;
+	/// <summary>
+	/// Поле идентификатора экземпляра.
+	/// </summary>
+	[ObservableProperty]
+    private uint _id;
 
-    /// <summary>
-    /// Поле имени контакта.
-    /// </summary>
+	/// <summary>
+	/// Поле имени контакта.
+	/// </summary>
+	[ObservableProperty]
     private string _name;
 
-    /// <summary>
-    /// Поле номера телефона контакта.
-    /// </summary>
+	/// <summary>
+	/// Поле номера телефона контакта.
+	/// </summary>
+	[ObservableProperty]
     private string _phoneNumber;
 
-    /// <summary>
-    /// Поле почты контакта.
-    /// </summary>
+	/// <summary>
+	/// Поле почты контакта.
+	/// </summary>
+	[ObservableProperty]
     private string _email;
 
     /// <summary>
     /// Статическое свойство, возвращающее количество созданных экземпляров класса.
     /// </summary>
-    private static int Created
+    private static uint Created
 	{
 		get => _created;
 		set
 		{
-                if (value - _created != 1)
-                {
+            if (value - _created != 1)
+            {
 				throw new ArgumentException($"{nameof(Created)} must be higher than new value precisely by 1");
-                }
-
-                _created = value;
-		}
-	}
-
-	/// <summary>
-	/// Свойство идентификатора экземпляра класса.
-	/// </summary>
-	public int Id
-	{
-		get => _id;
-		init 
-		{
-			if (value < 0)
-			{
-				throw new ArgumentException($"{nameof(Id)} must be higher than zero.");
-			}
-
-                Set(ref _id, value, nameof(Id));
             }
-	}
 
-	/// <summary>
-	/// Свойство имени контакта.
-	/// </summary>
-	public string Name
-	{
-		get => _name;
-		set 
-		{
-                Set(ref _name, value, nameof(Name));
-        }
-	}
-
-	/// <summary>
-	/// Свойство номера телефона контакта.
-	/// </summary>
-	public string PhoneNumber
-	{
-		get => _phoneNumber;
-		set 
-		{ 
-                Set(ref _phoneNumber, value, nameof(PhoneNumber));
-        }
-	}
-
-    /// <summary>
-    /// Свойство почты контакта.
-    /// </summary>
-    public string Email
-	{
-		get => _email;
-		set 
-		{
-			Set(ref _email, value, nameof(Email)); 
+            _created = value;
 		}
 	}
 
@@ -142,7 +81,7 @@ public class Contact : ICloneable, IEquatable<Contact>, INotifyPropertyChanged
 	/// <param name="temporaryPhoneNumberField"> Значение редактируемого поля номера телефона. </param>
 	/// <param name="temporaryEmailField"> Значение редактируемого поля почты. </param>
 	private Contact(
-		int id, 
+		uint id, 
 		string name = "No Name", 
 		string phoneNumber = "+7 (999) 999-99-99", 
 		string email = "email@gmail.com")
@@ -173,28 +112,6 @@ public class Contact : ICloneable, IEquatable<Contact>, INotifyPropertyChanged
 	/// <returns> true, если объеты равны, false - иначе. </returns>
     public bool Equals(Contact? other)
     {
-	return other.Id == Id;
-    }
-
-    /// <summary>
-    /// Функция задания нового уникального значения полю с уведомлением.
-    /// </summary>
-    /// <typeparam name="Type"> Тип поля. </typeparam>
-    /// <param name="fieldToSet"> Ссылка на поле, которому нужно задать значение. </param>
-    /// <param name="setValue"> Значение для задания. </param>
-    /// <param name="propertyName"> Имя свойства, в котором задается значение. </param>
-    /// <returns> Является ли значение новым для поля. (true - да, false - нет). </returns>
-    private bool Set<Type>(ref Type fieldToSet, Type setValue, [CallerMemberName] string propertyName = null)
-    {
-        if (Equals(fieldToSet, setValue))
-        {
-            return false;
-        }
-        else
-        {
-            fieldToSet = setValue;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
+		return other.Id == Id;
     }
 }
